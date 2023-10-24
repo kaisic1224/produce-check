0dbb221dc087f0980cb9eefa232815360c3e21a4
kaisic1224 <hiddenshadows1224@gmail.com>
2023-10-23 23:23:06 -0400

    fix input bpm, mood and fatigue after pause

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ LANG=C; LC_ALL=C
 
 function get_changes {
         declare -a args
-        args=(-type f -mmin -1 -not -path '*/.venv/*' -not -path '*/.git/*' -not -path '*/*data/*' -not -path "*.png" -not -path '*.lock' -not -path '*.gitignore' -not -path "*/lib/*" -not -path "*/target/*", -not -path "*/*.qgz" -not -path "*/*.pdf" -not -path "*/*.aux" -not -path "*/*.fdb_latexmk" -not -path "*/*.synctex.gz" -not -path "*/*.log" -not -path "*/*.fls" -not -path "*/*.log")
+        args=(-type f -mmin -10 -not -path '*/.venv/*' -not -path '*/.git/*' -not -path '*/*data/*' -not -path "*.png" -not -path '*.lock' -not -path '*.gitignore' -not -path "*/lib/*" -not -path "*/target/*", -not -path "*/*.qgz" -not -path "*/*.pdf" -not -path "*/*.aux" -not -path "*/*.fdb_latexmk" -not -path "*/*.synctex.gz" -not -path "*/*.log" -not -path "*/*.fls" -not -path "*/*.log" -not -path "*/*.rkt~")
         sum=0
         while IFS= read -r line; do
                 extension=${line##*.}
@@ -21,15 +21,16 @@ function get_changes {
 }
 
 function create_row {
-        get_changes
-        read -p "Please list your current mood (1-10 (best-worst)): " $mood
-        read -p "Please list your current fatigue (1-10 (least-most)): " $fatigue
-        read -p "Please list your current heartrate (bpm): " $bpm
-        printf '%s,%s,%s,%s,%s,%s\n' "$1" "$2" "$3" "$fatigue" "$mood" "$bpm" >> "../logs/log.csv"
+        productivity=$(get_changes)
+        read -p "Please list your current mood (1-10 (best-worst)): " mood
+        read -p "Please list your current fatigue (1-10 (least-most)): " fatigue
+        read -p "Please list your current heartrate (bpm): " bpm
+        printf '%s,%s,%s,%s,%s,%s,%s\n' "$1" "$2" "$3" "$productivity" "$fatigue" "$mood" "$bpm" >> "../logs/log.csv"
 }
 
 start_time=0
-while read -r line; do
+while read -u3 line; do
+        echo $line
         date=$(date +"%s")
         if [[ $line == "Playing" ]]; then
                 start_time=$date
@@ -37,6 +38,4 @@ while read -r line; do
                 difference="$(($date-$start_time))"
                 create_row $start_time $date $difference
         fi
-done < <(playerctl --follow status -p spotify)
-
-
+done 3< <(playerctl --follow status -p spotify)
