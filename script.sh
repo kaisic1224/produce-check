c6d1b5bd0e671b70bcbc2b0c04caadbfaa3bf4ed
kaisic1224 <hiddenshadows1224@gmail.com>
2023-10-24 23:13:58 -0400

    added changes in between down times

--- script.sh
+++ script.sh
@@ -3,7 +3,8 @@ LANG=C; LC_ALL=C
 
 function get_changes {
         declare -a args
-        args=(-type f -mmin -10 -not -path '*/.venv/*' -not -path '*/.git/*' -not -path '*/*data/*' -not -path "*.png" -not -path '*.lock' -not -path '*.gitignore' -not -path "*/lib/*" -not -path "*/target/*", -not -path "*/*.qgz" -not -path "*/*.pdf" -not -path "*/*.aux" -not -path "*/*.fdb_latexmk" -not -path "*/*.synctex.gz" -not -path "*/*.log" -not -path "*/*.fls" -not -path "*/*.log" -not -path "*/*.rkt~")
+        diff_in_secs=$(( "$1" / 60 ))
+        args=(-type f -mmin -"$diff_in_secs" -not -path '*/.venv/*' -not -path '*/.git/*' -not -path '*/*data/*' -not -path "*.png" -not -path '*.lock' -not -path '*.gitignore' -not -path "*/lib/*" -not -path "*/target/*", -not -path "*/*.qgz" -not -path "*/*.pdf" -not -path "*/*.aux" -not -path "*/*.fdb_latexmk" -not -path "*/*.synctex.gz" -not -path "*/*.log" -not -path "*/*.fls" -not -path "*/*.log" -not -path "*/*.rkt~")
         sum=0
         while IFS= read -r line; do
                 extension=${line##*.}
@@ -21,16 +22,24 @@ function get_changes {
 }
 
 function create_row {
-        productivity=$(get_changes)
+        productivity=$(get_changes $3)
         read -p "Please list your current mood (1-10 (best-worst)): " mood
         read -p "Please list your current fatigue (1-10 (least-most)): " fatigue
         read -p "Please list your current heartrate (bpm): " bpm
         printf '%s,%s,%s,%s,%s,%s,%s\n' "$1" "$2" "$3" "$productivity" "$fatigue" "$mood" "$bpm" >> "../logs/log.csv"
 }
 
+function sync_changes {
+        latest=$(tail -n 1 "../logs/log.csv")
+        IFS=',' read -ra record <<< "$latest"
+        current=$(date +"%s")
+        diff=$(($current - ${record[1]}))
+        get_changes $diff
+}
+
+sync_changes
 start_time=0
 while read -u3 line; do
-        echo $line
         date=$(date +"%s")
         if [[ $line == "Playing" ]]; then
                 start_time=$date
